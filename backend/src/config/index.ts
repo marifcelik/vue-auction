20cb05523252fbede6bf0db88a3cbc4e8309e26c
arif <arif-celik56@hotmail.com>
2023-03-18 00:02:39 +0300

    added cookie name

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,12 +3,13 @@ import { expand } from 'dotenv-expand';
 
 expand(dotenv.config());
 
-export const HOST = process.env.HOST ?? 'localhost',
+export const 
+  HOST = process.env.HOST ?? 'localhost',
   PORT = process.env.PORT ?? 5048,
   DB = process.env.DB ?? 'kartaca',
   DB_HOST = process.env.DB_HOST ?? '127.0.0.1',
-  DB_PORT = process.env.DB_PORT ?? 28014,
-  DB_CONN_STR = process.env.DB_CONN_STR ?? '',
+  DB_PORT = process.env.DB_PORT ?? 27017,
+  DB_CONN_STR = process.env.DB_CONN_STR ?? `mongodb://${DB_HOST}:${DB_PORT}/${DB}`,
   SECRET = process.env.SECRET ?? 'do not use secret like this',
   COOKIE_NAME = process.env.COOKIE_NAME ?? 'sessionid';
 
