a3a5f5d663ff35e37b09a744ae7d27fa0c3fa423
arif <arif-celik56@hotmail.com>
2023-03-18 00:03:17 +0300

    added auth controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { COOKIE_NAME } from '../config';
+import { logger } from '../utils/logger';
+
+class AuthController {
+  login(req: Request, res: Response) {
+    const { username, password } = req.body;
+    if (username === 'arif' && password === 'deneme') {
+      const user = { username, role: 'admin' };
+      req.session.user = user;
+      return res.sendStatus(200);
+    }
+    res.status(400).send({ msg: 'username or password is incorrect' });
+  }
+
+  check(req: Request, res: Response) {
+    logger.warn(req.session.id)
+    res.status(206).send('its okey');
+  }
+
+  logout(req: Request, res: Response) {
+    req.session.destroy((err) => {
+      if (err) logger.warn(err);
+    });
+    res.clearCookie(COOKIE_NAME);
+    res.sendStatus(200);
+  }
+}
+
+export default new AuthController();
