caf58d9136143337637fc2c62cc8546c8a861c3a
Mehmet Arif <arif-celik56@hotmail.com>
2023-03-19 20:40:28 +0300

    offer controller implemented

---
+++ backend/src/controllers/offer.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express"
+import Offer, { IOffer } from "../models/Offer.model";
+import jobHandler from "../utils/jobHandler";
+
+class OfferController {
+    async createOffer(req: Request, res: Response) {
+        const data = req.body as IOffer;
+        const offer = new Offer(data);
+
+        const [result, err] = await jobHandler(offer.save());
+        if (err) return res.status(500).json({ err });
+
+        return res.status(201).json({ result });
+    }
+
+    async getOffersByProductId(req: Request, res: Response) {
+        const productId = req.params.productId;
+
+        const [result, err] = await jobHandler(Offer.find({ _id: productId }));
+        if (err) return res.status(500).json({ err });
+
+        return result
+            ? res.status(200).json({ result })
+            : res.status(404).json({ message: 'Offer not found' });
+    }
+
+    async getOffersByUserId(req: Request, res: Response) {
+        const userId = req.params.userId;
+
+        const [result, err] = await jobHandler(Offer.find({ userId }));
+        if (err) return res.status(500).json({ err });
+
+        return result
+            ? res.status(200).json({ result })
+            : res.status(404).json({ message: 'Offer not found' });
+    }
+}
+
+export default new OfferController()
