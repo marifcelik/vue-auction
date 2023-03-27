1f8a85f974dd23b42a1a05ac8e09a7d84b0bd945
Mehmet Arif <arif-celik56@hotmail.com>
2023-03-27 15:37:54 +0300

    feat: serve static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import session from 'express-session';
 import { createServer } from 'http';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 import pino from './utils/logger';
 import router from './routers/router';
 import { redisStore } from './utils/db';
 import { COOKIE_MAXAGE, COOKIE_NAME, SECRET } from './config';
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const sessionParser = session({
   store: redisStore,
@@ -15,8 +18,9 @@ const sessionParser = session({
   rolling: true,
   saveUninitialized: false,
   secret: SECRET,
-  cookie: { maxAge: COOKIE_MAXAGE, sameSite: 'strict', httpOnly: false, secure: false }
+  cookie: { maxAge: COOKIE_MAXAGE, sameSite: 'none', httpOnly: false, secure: false }
 });
+const originList: (string | undefined)[] = ['http://localhost:5173', 'http://192.168.1.11:5173', 'http://192.168.1.3']
 
 // TODO: integrate swagger
 // import('swagger-ui-express')
@@ -28,7 +32,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(pino);
 app.use(sessionParser);
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: (origin, cb) => {
+    if (!origin || originList.indexOf(origin) !== -1)
+      cb(null, origin)
+    else
+      cb(new Error('not allowed by cors'))
+  },
+  credentials: true
+}));
+app.use('/img', express.static(resolve(__dirname, '../public/img')))
 
 app.use(router);
 
