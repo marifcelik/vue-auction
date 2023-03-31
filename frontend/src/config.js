c9942ccf6f15697a1f1c1c70fe41973f20cd0411
arif <arif-celik56@hotmail.com>
2023-03-31 20:26:23 +0300

    variable names updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const host = import.meta.env.VITE_HOST ?? 'http://localhost'
-const port = import.meta.env.VITE_PORT ?? '5048'
+const host = import.meta.env.VITE_SERVER_HOST ?? 'https://localhost'
+const port = import.meta.env.VITE_SERVER_PORT ?? '5048'
 
 export const SERVER = `${host}:${port}`
 export default { SERVER }
